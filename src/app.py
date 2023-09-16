71dd756c73e2611296a7bd6450b5e613776f7d09
Balázs Ádám Sidlóczki <sidbalazs18@gmail.com>
2023-09-16 12:21:11 +0200

    Create the database for the project

--- src/app.py
+++ src/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+from db_functions import *
 
 app = Flask(__name__)
 
+
+app.config['MYSQL_HOST'] = 'sql11.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql11646848'
+app.config['MYSQL_PASSWORD'] = 'h27FqgTnlq'
+app.config['MYSQL_DB'] = 'sql11646848'
+db_conn = DbConnections(app) 
 
 @app.route('/')
 def index():
     return render_template("main-menu.html")
 
+
+@app.route("/base")
+def base():
+	return render_template("base.html", word= db_conn.get_random_word())
 
 @app.route("/time")
 def time():
---
+++ src/db_functions.py
@@ -0,0 +1,24 @@
+from flask_mysqldb import MySQL
+import random
+
+
+class DbConnections:
+    def __init__(self, app) -> None:
+        self.app = app
+        self.mysql = MySQL(app)
+
+    def get_all_word(self):
+        cursor = self.mysql.connection.cursor()
+        cursor.execute('SELECT * FROM `Oddoneout`')
+        data = cursor.fetchall()
+        return data
+
+    def get_random_word(self):
+        cursor = self.mysql.connection.cursor()
+        cursor.execute('SELECT * FROM `Oddoneout`')
+        data = cursor.fetchall()
+        if data:
+            random_record = random.choice(data)
+            return random_record
+        else:
+            return None
