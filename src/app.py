d7d372b07abfbe9b9f03f5d2d3b0123277296474
Richard Poroszlay <richardporoszlay@gmail.com>
2023-09-22 18:29:54 +0200

    Reformat app.py

--- src/app.py
+++ src/app.py
@@ -5,10 +5,10 @@ from db_functions import *
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = 'sql11.freemysqlhosting.net'
-app.config['MYSQL_USER'] = 'sql11646848'
-app.config['MYSQL_PASSWORD'] = 'h27FqgTnlq'
-app.config['MYSQL_DB'] = 'sql11646848'
+app.config["MYSQL_HOST"] = "sql11.freemysqlhosting.net"
+app.config["MYSQL_USER"] = "sql11646848"
+app.config["MYSQL_PASSWORD"] = "h27FqgTnlq"
+app.config["MYSQL_DB"] = "sql11646848"
 db_conn = DbConnections(app)
 
 rounds_to_play = 0
@@ -20,89 +20,103 @@ hc_score = 0
 
 
 def get_solution(words):
-	global solution
-	solution = words[-1]
+    global solution
+    solution = words[-1]
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("main-menu.html")
 
 
 @app.route("/base")
 def base():
-	word = db_conn.get_random_record()
-	get_solution(word)
-	return render_template("base.html", word=word)
+    word = db_conn.get_random_record()
+    get_solution(word)
+    return render_template("base.html", word=word)
+
 
 @app.route("/base/<id>")
 def show_word(id):
-	if id == solution:
-		return render_template("won.html")
-	else:
-		return render_template("lost.html")
+    if id == solution:
+        return render_template("won.html")
+    else:
+        return render_template("lost.html")
+
 
 @app.route("/time")
 def time():
-	return render_template("time.html")
+    return render_template("time.html")
+
 
 @app.route("/input")
 def input():
-	return render_template("input.html")
+    return render_template("input.html")
 
-@app.route('/input_game', methods=['GET', 'POST'])
+
+@app.route("/input_game", methods=["GET", "POST"])
 def input_game():
-    global rounds_to_play  
+    global rounds_to_play
 
-    if request.method == 'POST':
-        rounds = int(request.form.get('rounds'))
+    if request.method == "POST":
+        rounds = int(request.form.get("rounds"))
         if rounds < 1:
-            return render_template('lost.html')
+            return render_template("lost.html")
         rounds_to_play = rounds
         word = db_conn.get_random_record()
         get_solution(word)
-        return render_template('input_game.html', word=word)
-    elif rounds_to_play-1 > 0:
+        return render_template("input_game.html", word=word)
+    elif rounds_to_play - 1 > 0:
         rounds_to_play -= 1
         word = db_conn.get_random_record()
         get_solution(word)
-        return render_template('input_game.html', word=word)
-    return redirect(url_for('game_result'))
+        return render_template("input_game.html", word=word)
+    return redirect(url_for("game_result"))
 
-@app.route('/input_game/<id>')
+
+@app.route("/input_game/<id>")
 def store_result(id):
-	global rounds_lost
-	global rounds_won
+    global rounds_lost
+    global rounds_won
 
-	if id == solution:
-		rounds_won += 1
-		return redirect(url_for('input_game'))
-	else:
-		rounds_lost += 1
-		return redirect(url_for('input_game'))
+    if id == solution:
+        rounds_won += 1
+        return redirect(url_for("input_game"))
+    else:
+        rounds_lost += 1
+        return redirect(url_for("input_game"))
 
-@app.route('/game_result')
+
+@app.route("/game_result")
 def game_result():
-    return render_template('input_game_result.html', rounds_won=rounds_won, rounds_lost=rounds_lost)
+    return render_template(
+        "input_game_result.html", rounds_won=rounds_won, rounds_lost=rounds_lost
+    )
+
 
 @app.route("/hardcore")
 def hardcore():
-	global hc_score
-	word = db_conn.get_random_record()
-	get_solution(word)
-	return render_template("hardcore.html", word=word, hc_score = hc_score)
+    global hc_score
+    word = db_conn.get_random_record()
+    get_solution(word)
+    return render_template("hardcore.html", word=word, hc_score=hc_score)
+
 
 @app.route("/hardcore/<id>")
 def show_next(id):
-	global hc_score
-	if id == solution:
-		hc_score += 1
-		print(hc_score)
-		return render_template("hardcore.html", word = db_conn.get_random_record(), hc_score=hc_score)
-	else:
-		print(hc_score)
-		temp_score = hc_score
-		hc_score = 0
-		return render_template("hc_lost.html", hc_score =temp_score)
+    global hc_score
+    if id == solution:
+        hc_score += 1
+        print(hc_score)
+        return render_template(
+            "hardcore.html", word=db_conn.get_random_record(), hc_score=hc_score
+        )
+    else:
+        print(hc_score)
+        temp_score = hc_score
+        hc_score = 0
+        return render_template("hc_lost.html", hc_score=temp_score)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
