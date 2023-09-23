bd1858bff915062bc4e35ab788198d064359ae5f
Richard Poroszlay <richardporoszlay@gmail.com>
2023-09-23 15:18:14 +0200

    Create timer.js for the countdown function in the time race mode

---
+++ src/static/timer.js
@@ -0,0 +1,27 @@
+const COUNTER_KEY = 'my-counter';
+
+function countDown(i, callback) {
+    //callback = callback || function(){};
+    timer = setInterval(function() {
+      seconds = parseInt(i % 15, 10);
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      document.getElementById("timer").innerHTML = "Time: " + seconds;
+
+      if ((i--) > 0) {
+        window.sessionStorage.setItem(COUNTER_KEY, i);
+      } else {
+        window.sessionStorage.removeItem(COUNTER_KEY);
+        document.location.href = "/tr-timeout";
+        clearInterval(timer);
+        callback();
+      }
+    }, 1000);
+}
+
+window.onload = function() {
+    var countDownTime = window.sessionStorage.getItem(COUNTER_KEY) || 15;
+    countDown(countDownTime, function() {
+      $('#myModal').modal('show');
+    });
+};
